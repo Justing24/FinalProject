dbe1b1375b9188ca776821378f953e82ecab6d65
Justing24 <ginesej1@newpaltz.edu>
2022-10-19 16:32:36 -0400

    Update Nav.vue

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -42,30 +42,6 @@ let isOpen = ref(false);
           Stats
         </router-link>
 
-
-        <!-- NOT IMIDIATLY NEEDED BUT IF I DO DECIDE TO DO DROPDOW ITS THERE -->
-
-        <!-- <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link">
-            More
-          </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item">
-              About
-            </a>
-            <a class="navbar-item">
-              Jobs
-            </a>
-            <a class="navbar-item">
-              Contact
-            </a>
-            <hr class="navbar-divider">
-            <a class="navbar-item">
-              Report an issue
-            </a>
-          </div>
-        </div> -->
       </div>
 
       <div class="navbar-end">
