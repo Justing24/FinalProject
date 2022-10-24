a9959e05a8dbd75087a5358392b1d4bfa7221597
Justing24 <ginesej1@newpaltz.edu>
2022-10-24 13:55:06 -0400

    Create Footer.vue

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<!-- Footer -->
+<footer class="footer-distributed">
+ 
+    <div class="footer-left">
+    
+    <h3>Muscle Booster</h3>
+    
+    <p class="footer-links">
+  
+    
+    <p class="footer-company-name">Muscle Booster</p>
+    </div>
+    
+    
+    
+    </footer>
+  </body>
