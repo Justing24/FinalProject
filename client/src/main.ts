73a01b20c65a5ece7653f4d184374e3593b3c846
Justing24 <ginesej1@newpaltz.edu>
2022-10-25 14:13:48 -0400

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,19 @@
+
+
 import { createApp } from "vue"
+import App from './App.vue'
+
+
 import { createPinia } from "pinia";
 
-import App from './App.vue'
+
 import router from './router'
 
 import './assets/main.scss'
 
 
 const app = createApp(App);
+
 
 app.use(createPinia());
 app.use(router);
